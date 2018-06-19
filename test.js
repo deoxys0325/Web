83481652d6539e28ceda1bb56fc4d8ef2ebd18ac
deoxys <f3256f3256@gmail.com>
2018-06-19 22:28:44 +0800

    小bug修復

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+
 // 顯示或隱藏子選單
 function switchMenu( theMainMenu, theSubMenu){
     var SubMenu = document.getElementById( theSubMenu );
@@ -91,63 +92,95 @@ function selectDeleteList( theList ){
         listOn = flase;
     }
 }
-var count=5;
+
 function addBook(){
+	var count = parseInt(localStorage.length/7);
     var bookname=document.getElementById("_bookname");
-    localStorage.setItem("NAME"+count,bookname.value);
     var isbn=document.getElementById("_ISBN");
-    localStorage.setItem("ISBN"+count,isbn.value);
-    var author=document.getElementById("_author");
-    localStorage.setItem("AUTHOR"+count,author.value);
-    var illustrator=document.getElementById("_illustrator");
-    localStorage.setItem("ILLUSTRATOR"+count,illustrator.value);
-    var price=document.getElementById("_price");
-    localStorage.setItem("PRICE"+count,price.value);
-    var publisher=document.getElementById("_publisher");
-    localStorage.setItem("PUBLISHER"+count,publisher.value);
-    var date=document.getElementById("_date");
-    localStorage.setItem("DATE"+count,date.value);
-    count++;
-	alert("已新增");
-	document.getElementById("_bookname").value = "";
-	document.getElementById("_ISBN").value = "";
-	document.getElementById("_author").value = "";
-	document.getElementById("_illustrator").value = "";
-	document.getElementById("_price").value = "";
-	document.getElementById("_publisher").value = "";
-	document.getElementById("_date").value = "";
+	var author=document.getElementById("_author");
+	var illustrator=document.getElementById("_illustrator");
+	var price=document.getElementById("_price");
+	var publisher=document.getElementById("_publisher");
+	var date=document.getElementById("_date");
+	if(bookname.value && isbn.value && author.value && illustrator.value && price.value && publisher.value && date.value){
+		localStorage.setItem("NAME"+count,bookname.value);
+		localStorage.setItem("ISBN"+count,isbn.value);
+		localStorage.setItem("AUTHOR"+count,author.value);
+		localStorage.setItem("ILLUSTRATOR"+count,illustrator.value);
+		localStorage.setItem("PRICE"+count,price.value);
+		localStorage.setItem("PUBLISHER"+count,publisher.value);
+		localStorage.setItem("DATE"+count,date.value);
+		count++;
+		alert("已新增");
+		document.getElementById("_bookname").value = "";
+		document.getElementById("_ISBN").value = "";
+		document.getElementById("_author").value = "";
+		document.getElementById("_illustrator").value = "";
+		document.getElementById("_price").value = "";
+		document.getElementById("_publisher").value = "";
+		document.getElementById("_date").value = "";
+	}
+	else{ alert("資料未填寫完整!!"); }
 }
+
 
 function deleteBook(){
 	var D = confirm("確定要將["+ document.getElementById('deleteBookName').value +"]刪除嗎?");
 	if(D){
-		var book;
-		for(var i = 0; i < localStorage.length/7;i++){
+		var isBookExist = false;
+		for(var i = 0; i < parseInt(localStorage.length/7);i++){
 			var a=i+5;
-			if(localStorage.getItem("NAME"+a) == document.getElementById("deleteBookName").value){
-				localStorage.removeItem("NAME"+a);
-				localStorage.removeItem("ISBN"+a);
-				localStorage.removeItem("AUTHOR"+a);
-				localStorage.removeItem("ILLUSTRATOR"+a);
-				localStorage.removeItem("PRICE"+a);
-				localStorage.removeItem("PUBLISHER"+a);
-				localStorage.removeItem("DATE"+a);
+			if(localStorage.getItem("NAME"+i) == document.getElementById("deleteBookName").value){
+				isBookExist = true;console.log(i);
+				localStorage.removeItem("NAME"+i);
+				localStorage.removeItem("ISBN"+i);
+				localStorage.removeItem("AUTHOR"+i);
+				localStorage.removeItem("ILLUSTRATOR"+i);
+				localStorage.removeItem("PRICE"+i);
+				localStorage.removeItem("PUBLISHER"+i);
+				localStorage.removeItem("DATE"+i);
+				if(localStorage.getItem("PICTURE"+i)) localStorage.removeItem("PICTURE"+i);
 				
+				if(localStorage.getItem("NAME"+(i+1))){
+					for(var j = i; localStorage.getItem("NAME"+(j+1)); j++ ){
+					localStorage.setItem("NAME"+j, localStorage.getItem("NAME"+(j+1)));
+					localStorage.setItem("ISBN"+j, localStorage.getItem("ISBN"+(j+1)));
+					localStorage.setItem("AUTHOR"+j, localStorage.getItem("AUTHOR"+(j+1)));
+					localStorage.setItem("ILLUSTRATOR"+j, localStorage.getItem("ILLUSTRATOR"+(j+1)));
+					localStorage.setItem("PRICE"+j, localStorage.getItem("PRICE"+(j+1)));
+					localStorage.setItem("PUBLISHER"+j, localStorage.getItem("PUBLISHER"+(j+1)));
+					localStorage.setItem("DATE"+j, localStorage.getItem("DATE"+(j+1)));
+					if(localStorage.getItem("PICTURE"+(j+1))) localStorage.setItem("PICTURE"+j, localStorage.getItem("PICTURE"+(j+1)));
+					else localStorage.removeItem("PICTURE"+j);
+					document.getElementById("book"+j).innerHTML = "<img src='images/book"+(j+1)+".jpg' height='200px'>";
+					}
+					localStorage.removeItem("NAME"+j);
+					localStorage.removeItem("ISBN"+j);
+					localStorage.removeItem("AUTHOR"+j);
+					localStorage.removeItem("ILLUSTRATOR"+j);
+					localStorage.removeItem("PRICE"+j);
+					localStorage.removeItem("PUBLISHER"+j);
+					localStorage.removeItem("DATE"+j);
+					if(localStorage.getItem("PICTURE"+j)) localStorage.removeItem("PICTURE"+j);
+				}
 			}
 		}
+		if(!isBookExist){ alert("資料庫中沒有這本書唷~"); }
 		location.reload();
 	}
 }
+
+
 function researchBookByBookname(){
     var words=document.getElementById("research").value;
     if(words!=""){
-    for(var i = 0; i < localStorage.length/7;i++){
+    for(var i = 0; i < parseInt(localStorage.length/7);i++){
             var a=i+5;
-            if(localStorage.getItem("NAME"+a) != words){
-                $("#List"+a).hide();
+            if(localStorage.getItem("NAME"+i) != words){
+                $("#List"+i).hide();
             }
-              if(localStorage.getItem("NAME"+a) == words){
-                $("#List"+a).show();
+              if(localStorage.getItem("NAME"+i) == words){
+                $("#List"+i).show();
             }
         }
     }
@@ -155,13 +188,13 @@ function researchBookByBookname(){
 function researchBookByBookauthor(){
     var words=document.getElementById("research").value;
     if(words!=""){
-    for(var i = 0; i < localStorage.length/7;i++){
+    for(var i = 0; i < parseInt(localStorage.length/7);i++){
             var a=i+5;
-            if(localStorage.getItem("AUTHOR"+a) != words){
-                $("#List"+a).hide();
+            if(localStorage.getItem("AUTHOR"+i) != words){
+                $("#List"+i).hide();
             }
-              if(localStorage.getItem("AUTHOR"+a) == words){
-                $("#List"+a).show();
+              if(localStorage.getItem("AUTHOR"+i) == words){
+                $("#List"+i).show();
             }
         }
     }
@@ -169,13 +202,13 @@ function researchBookByBookauthor(){
 function researchBookByBookprice(){
     var words=document.getElementById("research").value;
     if(words!=""){
-    for(var i = 0; i < localStorage.length/7;i++){
+    for(var i = 0; i < parseInt(localStorage.length/7);i++){
             var a=i+5;
-            if(localStorage.getItem("PRICE"+a) != words){
-                $("#List"+a).hide();
+            if(localStorage.getItem("PRICE"+i) != words){
+                $("#List"+i).hide();
             }
-              if(localStorage.getItem("PRICE"+a) == words){
-                $("#List"+a).show();
+              if(localStorage.getItem("PRICE"+i) == words){
+                $("#List"+i).show();
             }
         }
     }
@@ -183,17 +216,18 @@ function researchBookByBookprice(){
 function researchBookByBookdate(){
     var words=document.getElementById("research").value;
     if(words!=""){
-    for(var i = 0; i < localStorage.length/7;i++){
+    for(var i = 0; i < parseInt(localStorage.length/7);i++){
             var a=i+5;
-            if(localStorage.getItem("PRICE"+a) != words){
-                $("#List"+a).hide();
+            if(localStorage.getItem("PRICE"+i) != words){
+                $("#List"+i).hide();
             }
-              if(localStorage.getItem("PRICE"+a) == words){
-                $("#List"+a).show();
+              if(localStorage.getItem("PRICE"+i) == words){
+                $("#List"+i).show();
             }
         }
     }
 }
+
 function scrolltop(){
     window,document.body.scrollTop=0;
 }
